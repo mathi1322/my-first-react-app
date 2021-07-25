c6e357e0ca77302cb06ad6fafe34545f1f6640a3
mathi1322 <mathivanan@drylogics.com>
2021-07-25 17:10:21 +0530

    code worked for counter

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
   const increaseCount = () => setCount(prevCount => prevCount + 1);
+  const decreaseCount = () => {
+    if (count > 0) {
+      setCount(prevCount => prevCount - 1);
+    }
+  }
+  const resetCount = () => setCount(0);
   return (
     <div className="App">
       <h1> Click Count: {count}</h1>
       <button onClick={increaseCount}> Increase </button>
-      <button> Decrease </button>
-      <button> Reset </button>
+      <button onClick={decreaseCount}> Decrease </button>
+      <button onClick={resetCount}> Reset </button>
     </div>
   );
 }
