3e57ca5e2a4560ab007fe29e97aa123f8677e3d7
mathi1322 <mathivanan@drylogics.com>
2021-07-25 17:00:28 +0530

    make_working_increaseCount

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const increaseCount = () => setCount(prevCount => prevCount + 1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Click Count: {count}</h1>
+      <button onClick={increaseCount}> Increase </button>
+      <button> Decrease </button>
+      <button> Reset </button>
     </div>
   );
 }
